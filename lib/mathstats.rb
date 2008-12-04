56b9f2b759020bc7f03f6819bbb81e50756e7838
Ben Marini <ben@ben-marinis-powerbook-g4-15.local>
2008-12-03 19:19:41 -0800

    Added sum method

--- lib/mathstats.rb
+++ lib/mathstats.rb
@@ -3,16 +3,24 @@ module Mathstats
     Lib.average(self, identity, &block)
   end
   
+  def sum(identity = 0, &block)
+    Lib.sum(self, identity, &block)
+  end
+  
   class Lib
     def self.average(array, identity = 0, &block)
+      array.size > 0 ? sum(array, identity, &block) / array.size.to_f : identity
+    end
+
+    def self.sum(array, identity = 0, &block)
       return identity unless array.size > 0
 
-
       if block_given?
-        average array.map(&block)
+        sum( array.map(&block) )
       else
-        array.inject { |sum, element| sum + element } / array.size.to_f
+        array.inject { |sum, element| sum + element }
       end
     end
+   
   end
 end
--- test/mathstats_test.rb
+++ test/mathstats_test.rb
@@ -4,8 +4,7 @@ require 'mathstats'
 class TC_MyTest < Test::Unit::TestCase
   # def setup
   # end
-
-  def test_as_module
+  def test_average_as_module
     Array.send :include, Mathstats
     assert_equal 3, [1,2,3,4,5].average
     assert_equal(6, [1,2,3,4,5].average {|n| n * 2 })
@@ -15,8 +14,30 @@ class TC_MyTest < Test::Unit::TestCase
     assert_equal(1.5, [1,2].average)
   end
   
-  def test_as_class
+  def test_average_as_class
     assert_equal 3, Mathstats::Lib.average([1,2,3,4,5])
+    assert_equal(6, Mathstats::Lib.average([1,2,3,4,5]) {|n| n * 2 })
+    assert_equal(0, Mathstats::Lib.average([], 0))
+    assert_equal('empty', Mathstats::Lib.average([], 'empty'))
+    
+    assert_equal(1.5, Mathstats::Lib.average([1,2]))
+  end
+
+  def test_sum_as_module
+    Array.send :include, Mathstats
+    assert_equal 10, [1,2,3,4].sum
+    assert_equal(20, [1,2,3,4].sum {|n| n * 2 })
+    assert_equal(0, [].sum(0))
+    assert_equal('empty', [].sum('empty'))
+  end
+
+  def test_sum_as_class
+    assert_equal 15, Mathstats::Lib.sum([1,2,3,4,5])
+    assert_equal(30, Mathstats::Lib.sum([1,2,3,4,5]) {|n| n * 2 })
+    assert_equal(0, Mathstats::Lib.sum([], 0))
+    assert_equal('empty', Mathstats::Lib.sum([], 'empty'))
+    
+    assert_equal(3, Mathstats::Lib.sum([1,2]))
   end
 
   # def teardown
